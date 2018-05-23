f7c09c486e15fe37f1b43cb413709b32394502c2
Samuel Masters <redeyetuning@gmail.com>
2018-05-23 13:06:05 +0400

    Add basic New User HTML form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,5 @@ class User < ApplicationRecord
 	validates :username, presence: true
 	validates :email, presence: true
 
-
-	has_secure_password
 	validates :password, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20180522174231) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password_digest"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
