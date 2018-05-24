cb4956b7966523a8f52d7d0402c5e978a1cbb152
Samuel Masters <redeyetuning@gmail.com>
2018-05-24 12:39:17 +0400

    Add Index,Show,Edit,Update routes with Views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+	def index
+		@users = User.all
+	end
 
 	def create
 		#@user = User.new params[:user]
@@ -11,6 +14,23 @@ class UsersController < ApplicationController
 			redirect_to new_user_path
 		else
 			render :new
+		end
+	end
+
+	def show
+		@user = User.find params[:id]
+	end
+
+	def edit
+		@user = User.find params[:id]
+	end
+
+	def update
+		@user = User.find params[:id]
+		if @user.update_attributes user_params
+			redirect_to user_path
+		else
+			render :edit
 		end
 	end
 
